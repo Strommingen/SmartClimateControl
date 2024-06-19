f11ae4ccdb59fb03ba998aae251069dd8120402b
Strommingen <112416297+Strommingen@users.noreply.github.com>
2024-06-19 09:38:52 +0200

    logic fixes with temperature comparisons

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ def getHumidity(sensor) -> float:
         print("Something went wrong while reading humidity..", err)
 
 def windowShouldOpen(humOut, humIn, tempOut, tempIn) -> bool:
-    if tempOut >= 17 or tempIn > 24:
-        if tempIn > tempOut:
-            return True
+    if (tempOut >= 17) and (tempIn >= 24) and (tempOut > tempIn):
+        return True
         # Humidity conditions
-        if (humOut > humIn and humIn < 0.4) or (humOut < humIn and humIn > 0.5):
-            return True
+    if (humOut > humIn and humIn < 0.4) or (humOut < humIn and humIn > 0.5):
+        return True
     # If none of the conditions met, return False
     return False
 #https://lauryheating.com/ideal-home-humidity/
@@ -46,7 +45,6 @@ client = mqtt.MQTTClient(keys.AIO_CLIENT_ID, keys.AIO_SERVER, keys.AIO_PORT, key
 client.connect()
 print(f"Connected to {keys.AIO_SERVER}")
 
-msg=''
 nextNotice = time.mktime(time.localtime()) # current time since epoch
 actionNeeded = False
 
@@ -64,44 +62,40 @@ try:
         tempIn = getTemperature(dhtInside)
         humIn = getHumidity(dhtInside)
         
-        windowIsOpen = windowPin.value()
-
-        # if the window should open
-        if windowShouldOpen(humOut, humIn, tempOut, tempIn):
-            if not windowIsOpen:
-                actionNeeded=True
-                msg='Closed'
-                led.on()
-            else:
-                led.off()
-                msg='Open'
-        # window should not open
+        windowState=''
+        if windowPin.value():
+            windowState = 'Open'
         else:
-            if windowIsOpen:
-                actionNeeded= True
-                msg ='Open'
-                led.On()
-            else:
-                led.off()
-                msg='Closed'
-            
+            windowState = 'Closed'
+
+        windowOpenDesired = windowShouldOpen(humOut, humIn, tempOut, tempIn)
+
+        if windowOpenDesired and windowState == 'Closed':
+            actionNeeded=True
+            led.on()
+        elif not windowOpenDesired and windowState == 'Open':
+            actionNeeded = True
+            led.on()
+        else:
+            actionNeeded=False
+            led.off()
 
         # we only send data every 45 minutes
         if nextNotice-time.mktime(time.localtime()) < 0:
             if actionNeeded: # and only send to window feed if action is needed
-                client.publish(keys.AIO_WINDOW_FEED, msg)
+                client.publish(keys.AIO_WINDOW_FEED, windowState)
             client.publish(keys.AIO_TEMPOUT_FEED, str(tempOut))
             client.publish(keys.AIO_TEMPIN_FEED, str(tempIn))
             client.publish(keys.AIO_HUMIN_FEED, str(humIn))
             client.publish(keys.AIO_HUMOUT_FEED, str(humOut))
             print('Published!')
-            nextNotice = time.mktime(time.localtime()) +2700
+            nextNotice = time.mktime(time.localtime()) +900
 
         print(f'Outside {tempOut} C, {humOut} %')
         print(f'Inside {tempIn} C, {humIn} %')
-        print(f'Window is {msg}')
+        print(f'Window is {windowState}')
         print(f'Time until next publish: {nextNotice-time.mktime(time.localtime())} \n')
-        time.sleep(30)
+        time.sleep(10)
 finally:
     client.disconnect()
     client = None
